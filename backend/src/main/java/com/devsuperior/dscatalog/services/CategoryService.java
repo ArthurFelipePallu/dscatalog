aa299f893c7ec0cd621f04d2f7b2cd74e950fbd7
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-05-24 14:53:23 -0300

    H2 database funcionando

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+/*Anotation registra a classe como um componente que participa da injeção de dependencias automatizado do spring,
+ * quem vai gerenciar as instancias da injeção de dependencia será o próprio spring
+ * */
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository ;
+	
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
