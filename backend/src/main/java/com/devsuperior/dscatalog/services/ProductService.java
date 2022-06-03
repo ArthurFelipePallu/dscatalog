634cb8323e4407d6ed93a6521c3cd77a25aa69ba
Arthur Pallu <arthurfelipepallu@gmail.com>
2022-06-03 00:37:04 -0300

    Product insert,update,delete

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -26,6 +29,8 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository ;
+	@Autowired
+	private CategoryRepository category_repository;
 	
 	/*@Transactional(readOnly = true)        ANTIGO
 	public List<CategoryDTO> findAll(){
@@ -51,18 +56,16 @@ public class ProductService {
 	@Transactional(readOnly = true)
 	public ProductDTO insert(ProductDTO dto) {
 		Product product = new Product();
-		/*product.setName(dto.getName());
-		product.setName(dto.getName());
-		product.setName(dto.getName());
-		product.setName(dto.getName());*/
+		copyDtoToEntity(dto,product);
 		product = repository.save(product);
 		return new ProductDTO(product);
 	}
+
 	@Transactional
 	public ProductDTO update(Long id,ProductDTO dto) {
 		try {
 			Product product = repository.getReferenceById(id);
-			//product.setName(dto.getName());
+			copyDtoToEntity(dto,product);
 			product = repository.save(product);
 			return new ProductDTO(product);
 		}
@@ -82,4 +85,20 @@ public class ProductService {
 			throw new DatabaseException("Integrity Violation");
 		}
 	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product product) {
+		product.setName(dto.getName());
+		product.setDescription(dto.getDescription());
+		product.setDate(dto.getDate());
+		product.setImgUrl(dto.getImgUrl());
+		product.setPrice(dto.getPrice());
+		
+		product.getCategories().clear();
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category cat = category_repository.getReferenceById(catDto.getId());
+			product.getCategories().add(cat);
+		}		
+	}
+	
+	
 }
